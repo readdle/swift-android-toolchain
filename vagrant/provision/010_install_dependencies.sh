88ecc3517f614335a0244e3cfb74759c523e3f61
Anton Pogonets <apogonets@readdle.com>
2018-01-11 15:07:22 +0200

    Add rpl to provision

--- vagrant/provision/010_install_dependencies.sh
+++ vagrant/provision/010_install_dependencies.sh
@@ -4,6 +4,6 @@ apt-get install -y \
     git cmake ninja-build clang python uuid-dev libicu-dev icu-devtools \
     libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev \
     libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev \
-    autoconf automake libtool curl wget unzip vim
+    autoconf automake libtool curl wget unzip vim rpl
 
 ln -s /usr/bin/perl /usr/local/bin/perl
