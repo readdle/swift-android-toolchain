f325beb76faa616e87a4b9b2e84c72dade452c6e
Anton Pogonets <apogonets@readdle.com>
2018-11-03 04:49:03 +0200

    copy swiftmodule when rebuild foundation

--- build/rebuild_foundation.sh
+++ build/rebuild_foundation.sh
@@ -5,6 +5,13 @@ SELF_DIR=$(dirname $0)
 
 pushd $SELF_DIR/../vagrant
     vagrant ssh -c /vagrant/scripts/060_build_corelibs_foundation.sh
-    vagrant scp :~/swift-source/build/Ninja-ReleaseAssert+stdlib-Release/foundation-linux-x86_64/Foundation/libFoundation.so $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/
+
+    vagrant scp :~/swift-source/build/Ninja-ReleaseAssert+stdlib-Release/foundation-linux-x86_64/Foundation/libFoundation.so \
+        $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/
+    vagrant scp :~/swift-source/build/Ninja-ReleaseAssert+stdlib-Release/foundation-linux-x86_64/Foundation/Foundation.swiftmodule \
+        $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/armv7/
+    vagrant scp :~/swift-source/build/Ninja-ReleaseAssert+stdlib-Release/foundation-linux-x86_64/Foundation/Foundation.swiftdoc \
+        $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/armv7/
+
     rpl -R -e libicu libscu $SWIFT_ANDROID_HOME/toolchain/usr/lib/swift/android/libFoundation.so
 popd
