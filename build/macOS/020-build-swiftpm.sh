0226239f87f1e91d5a2a55fbb57fad2300aeeb28
Anton Pogonets <apogonets@readdle.com>
2021-04-09 21:43:48 +0300

    Fix swiftpm build script

--- build/macOS/020-build-swiftpm.sh
+++ build/macOS/020-build-swiftpm.sh
@@ -2,7 +2,7 @@
 set -ex
 
 BASE_DIR=`pwd`
-SWIFT_SOURCE=$BASE_DIR/vagrant/out/swift-source`
+SWIFT_SOURCE=$BASE_DIR/vagrant/out/swift-source
 
 pushd $SWIFT_SOURCE/swiftpm
     swift build --configuration release --product swift-build
