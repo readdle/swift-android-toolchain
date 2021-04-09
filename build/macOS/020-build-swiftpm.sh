902b99822188070c9701d3bd8553834de708459e
Anton Pogonets <apogonets@readdle.com>
2021-04-09 21:07:42 +0300

    Dont ignore custom toolchain when build swiftpm

--- build/macOS/020-build-swiftpm.sh
+++ build/macOS/020-build-swiftpm.sh
@@ -2,9 +2,7 @@
 set -ex
 
 BASE_DIR=`pwd`
-SWIFT_SOURCE=$BASE_DIR/vagrant/out/swift-source
-
-unset TOOLCHAINS
+SWIFT_SOURCE=$BASE_DIR/vagrant/out/swift-source`
 
 pushd $SWIFT_SOURCE/swiftpm
     swift build --configuration release --product swift-build
