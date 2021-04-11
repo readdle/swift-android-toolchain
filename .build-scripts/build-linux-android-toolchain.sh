791ee276a2338bcc8618d297a72351b8bd2d6888
Andrew Druk <adruk@readdle.com>
2021-04-11 16:31:17 +0300

    Dev: fix folder of script execution

--- .build-scripts/build-linux-android-toolchain.sh
+++ .build-scripts/build-linux-android-toolchain.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -ex
 
-SELF_DIR=$(dirname $0)
-BASE_DIR=$SELF_DIR/../
+BASE_DIR=$PWD
 
+# Provisioning
+pushd $HOME
+	$BASE_DIR/vagrant/provision/010_install_dependencies.sh
+	$BASE_DIR/vagrant/provision/020_install_ndk.sh
+	$BASE_DIR/vagrant/provision/030_define_build_folders.sh
+popd
+
+# Building
 pushd $BASE_DIR
-	# Provisioning
-	./vagrant/provision/010_install_dependencies.sh
-	./vagrant/provision/020_install_ndk.sh
-	./vagrant/provision/030_define_build_folders.sh
-
-	# Building
 	./build/Linux/010-build-icu.sh
     ./build/Linux/020-clone-swift.sh
     ./build/Linux/030-build-swift.sh
