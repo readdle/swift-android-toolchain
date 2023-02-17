2de4d64db5f080abd22d86be5aedcb6c07eb839c
Andrew Druk <adruk@readdle.com>
2023-02-17 18:04:45 +0200

    Dev: skip glibc modulemap path

--- build/Linux/060-collect-toolchain.sh
+++ build/Linux/060-collect-toolchain.sh
@@ -17,32 +17,11 @@ input_bin=$HOME/bin
 input_libs=$HOME/lib
 
 pushd $out
-
     # Copy bin from mac os toolchain
     rsync -av $input_bin $out_toolchain/usr
 
     # Copy platform libs
     rsync -av $input_libs $out_toolchain/usr --exclude 'lib/clang/10.0.0/lib'
-
-    # Bundle NDK headers
-    mkdir -p $out_toolchain/ndk-android-21/usr
-    rsync -av $ANDROID_NDK/sysroot/usr/include $out_toolchain/ndk-android-21/usr
-
-    # Patch Glibc module
-    for swift_arch in aarch64 armv7 x86_64 i686
-    do
-        ls -la $out_toolchain/usr/lib/
-        ls -la $out_toolchain/usr/lib/swift-$swift_arch/android/
-        ls -la $out_toolchain/usr/lib/swift-$swift_arch/android/$swift_arch
-        glibc_modulemap="$out_toolchain/usr/lib/swift-$swift_arch/android/$swift_arch/glibc.modulemap"
-
-        if [[ ! -f "$glibc_modulemap.orig" ]]
-        then
-            cp "$glibc_modulemap" "$glibc_modulemap.orig"
-        fi
-
-        sed -e 's@/home/runner/android-ndk-r21e/toolchains/llvm/prebuilt/linux-x86_64/sysroot@../../../../../ndk-android-21@' < "$glibc_modulemap.orig" > "$glibc_modulemap"
-    done
 popd
 
 # Move libIndexStore.dylib from bin to lib
