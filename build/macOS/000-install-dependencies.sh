792434edbd672437eeca6abf47185123d6b85527
Andrew Druk <adruk@readdle.com>
2023-10-20 16:15:15 +0300

    Dev: migrate to Python3

--- build/macOS/000-install-dependencies.sh
+++ build/macOS/000-install-dependencies.sh
@@ -6,7 +6,7 @@ brew install coreutils cmake ninja pkg-config
 
 # Prepare Python
 python --version
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
+curl https://bootstrap.pypa.io/pip/get-pip.py -o get-pip.py
 python get-pip.py
 pip install six
 
