3c6d02e5065a847957e919cdcbcb47c6baf2b420
Andrew Druk <adruk@readdle.com>
2024-06-04 23:36:24 +0300

    Dev: update clang to version 13

--- build/Linux/000-install-dependencies.sh
+++ build/Linux/000-install-dependencies.sh
@@ -31,13 +31,25 @@ apt-get -y update && DEBIAN_FRONTEND=noninteractive apt-get -y install \
   zip
 
 apt-get install -y  \
-    clang           \
-    autoconf        \
-    automake        \
-    libtool         \
-    curl            \
-    wget            \
-    unzip           \
-    vim             \
+    lsb-release                 \
+    software-properties-common  \
+    gnupg                       \
+    autoconf                    \
+    automake                    \
+    libtool                     \
+    curl                        \
+    wget                        \
+    unzip                       \
+    vim                         \
     rpl
 
+# Install clang 13
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
+add-apt-repository "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-13 main"
+apt-get update
+apt-get install -y clang-13
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-13 100
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-13 100
+update-alternatives --set clang /usr/bin/clang-13
+update-alternatives --set clang++ /usr/bin/clang++-13
+clang --version
