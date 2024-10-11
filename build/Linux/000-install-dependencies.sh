290c980af97ab6c7d41dcecb43d071dd501b7b20
Andrew Druk <adruk@readdle.com>
2024-10-11 11:56:39 +0300

    Dev: bootstrap Swift 6.0.1

--- build/Linux/000-install-dependencies.sh
+++ build/Linux/000-install-dependencies.sh
@@ -57,11 +57,11 @@ update-alternatives --set clang++ /usr/bin/clang++-13
 clang --version
 
 # Install swift for bootstraping
-wget https://download.swift.org/swift-5.10-release/ubuntu2004/swift-5.10-RELEASE/swift-5.10-RELEASE-ubuntu20.04.tar.gz
-tar -xvzf swift-5.10-RELEASE-ubuntu20.04.tar.gz
-rm swift-5.10-RELEASE-ubuntu20.04.tar.gz
-export PATH=$HOME/swift-5.10-RELEASE-ubuntu20.04/usr/bin:$PATH
-echo "export PATH=\$HOME/swift-5.10-RELEASE-ubuntu20.04/usr/bin:\$PATH" >> .build_env
+wget https://download.swift.org/swift-6.0.1-release/ubuntu2004/swift-6.0.1-RELEASE/swift-6.0.1-RELEASE-ubuntu20.04.tar.gz
+tar -xvzf swift-6.0.1-RELEASE-ubuntu20.04.tar.gz
+rm swift-6.0.1-RELEASE-ubuntu20.04.tar.gz
+export PATH=$HOME/swift-6.0.1-RELEASE-ubuntu20.04/usr/bin:$PATH
+echo "export PATH=\$HOME/swift-6.0.1-RELEASE-ubuntu20.04/usr/bin:\$PATH" >> .build_env
 
 swift --version
   
