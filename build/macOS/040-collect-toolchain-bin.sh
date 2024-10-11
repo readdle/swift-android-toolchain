01d12f54b79e73999cc89e1ccb24807ebd4f4e93
Andrew Druk <adruk@readdle.com>
2024-10-11 19:21:19 +0300

    Dev: fix switch macOS from x86_64 to arm64

--- build/macOS/040-collect-toolchain-bin.sh
+++ build/macOS/040-collect-toolchain-bin.sh
@@ -9,9 +9,9 @@ out_bin=~/bin
 mkdir -p $out_bin
 
 cp -f $SWIFT_SRC/swiftpm/.build/release/swift-build $out_bin
-cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/llvm-macosx-x86_64/lib/libIndexStore.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/llvm-macosx-arm64/lib/libIndexStore.dylib $out_bin
 
-rsync -av $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-x86_64/bin/ $out_bin \
+rsync -av $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/bin/ $out_bin \
         --include swift \
         --include swift-autolink-extract \
         --include swift-stdlib-tool \
