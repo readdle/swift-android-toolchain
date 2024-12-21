d64119e7b10796eb07133c683c64b5f61ee77c65
Andrew Druk <adruk@readdle.com>
2024-12-21 17:31:49 +0200

    Dev: patch NDK fts_open function in fts.h header

--- build/Linux/002-install-ndk.sh
+++ build/Linux/002-install-ndk.sh
@@ -2,17 +2,21 @@
 
 cd $HOME
 
-for version in 26c
-do
-    ndk=android-ndk-r$version
-    ndk_zip=$ndk-linux.zip
+ndk=android-ndk-r26c
+ndk_zip=$ndk-linux.zip
 
-    wget --progress=bar:force https://dl.google.com/android/repository/$ndk_zip
+wget --progress=bar:force https://dl.google.com/android/repository/$ndk_zip
+unzip $ndk_zip
+rm $ndk_zip
 
-    unzip $ndk_zip
-    rm $ndk_zip
-done
+# Patch: Fix `fts_open` Parameter Annotation in NDK `fts.h`
+#
+# In the Android NDK 26c the function `fts_open` is incorrectly
+# declared to accept `char* _Nonnull const* _Nonnull` for its first parameter.
+# However, the array should actually be null-terminated, indicating the pointer
+# at the end may be NULL. Therefore, `_Nullable` is more appropriate.
+sed -i 's/fts_open(char\* _Nonnull const\* _Nonnull/fts_open(char* _Nullable const* _Nonnull/' $ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/include/fts.h
 
 # exports
-echo "export ANDROID_NDK=\$HOME/android-ndk-r26c" >> .build_env
+echo "export ANDROID_NDK=\$HOME/$ndk" >> .build_env
 echo "export ANDROID_NDK_HOME=\$ANDROID_NDK" >> .build_env
