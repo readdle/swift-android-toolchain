fa21879f3016cfe9836ac3c9f955322564b49e57
Andriy Druk <andriy.druk@gmail.com>
2025-01-19 18:03:57 +0200

    Fix macOS toolchain collecting

--- build/macOS/040-collect-toolchain-bin.sh
+++ build/macOS/040-collect-toolchain-bin.sh
@@ -10,6 +10,22 @@ mkdir -p $out_bin
 
 cp -f $SWIFT_SRC/swiftpm/.build/release/swift-build $out_bin
 cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/llvm-macosx-arm64/lib/libIndexStore.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftBasicFormat.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftCompilerPluginMessageHandling.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftDiagnostics.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftIDEUtils.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftOperators.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftParser.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftParserDiagnostics.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftSyntax.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftSyntaxBuilder.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftSyntaxMacroExpansion.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/lib/swift/host/libSwiftSyntaxMacros.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/earlyswiftdriver-macosx-arm64/release/lib/libSwiftDriver.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/earlyswiftdriver-macosx-arm64/release/lib/libSwiftDriverExecution.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/earlyswiftdriver-macosx-arm64/release/lib/libSwiftOptions.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/earlyswiftdriver-macosx-arm64/release/dependencies/swift-tools-support-core/lib/libTSCBasic.dylib $out_bin
+cp -f $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/earlyswiftdriver-macosx-arm64/release/dependencies/swift-tools-support-core/lib/libTSCUtility.dylib $out_bin
 
 rsync -av $SWIFT_SRC/build/Ninja-ReleaseAssert+stdlib-Release/swift-macosx-arm64/bin/ $out_bin \
         --include swift \
