b42bc2cb8a08007943a138945a90ee52cc981bd6
Andrew Druk <adruk@readdle.com>
2025-09-28 17:26:16 +0300

    Update install dependencies for ubuntu 22.04 and swift 6.2

--- build/000-install-dependencies-nobel.sh
+++ build/000-install-dependencies-nobel.sh
@@ -6,34 +6,24 @@ cd $HOME
 # Copied from https://github.com/swiftlang/swift-docker/blob/main/swift-ci/main/ubuntu/24.04/Dockerfile
 DEBIAN_FRONTEND="noninteractive"
 
-apt-get -y update && apt-get -y install \
-  build-essential       \
-  clang                 \
-  cmake                 \
-  git                   \
-  icu-devtools          \
-  libc++-18-dev         \
-  libc++abi-18-dev      \
-  libcurl4-openssl-dev  \
-  libedit-dev           \
-  libicu-dev            \
-  libncurses5-dev       \
-  libpython3-dev        \
-  libsqlite3-dev        \
-  libxml2-dev           \
-  ninja-build           \
-  pkg-config            \
-  python3-six           \
-  python3-pip           \
-  python3-pkg-resources \
-  python3-psutil        \
-  python3-setuptools    \
-  rsync                 \
-  swig                  \
-  systemtap-sdt-dev     \
-  tzdata                \
-  uuid-dev              \
-  zip
+apt-get -y update && apt-get -q install -y \
+    binutils \
+    git \
+    unzip \
+    gnupg2 \
+    libc6-dev \
+    libcurl4-openssl-dev \
+    libedit2 \
+    libgcc-13-dev \
+    libpython3-dev \
+    libsqlite3-0 \
+    libstdc++-13-dev \
+    libxml2-dev \
+    libncurses-dev \
+    libz3-dev \
+    pkg-config \
+    tzdata \
+    zlib1g-dev
 
 apt-get install -y  \
     lsb-release                 \
